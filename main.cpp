fcd25b069c80923ec376123c1bf97645c1642bf5
Kyuwoong Lee <kwooler@gmail.com>
2023-08-27 18:15:26 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main()
+{
+  double C;
+  double F;
+  
+  /* ************************************
+    Code your program here
+  ************************************ */
+
+  cout << setprecision(2) << fixed;
+  cout << "Celcius " << C << " is " << F << " Fahrenhiet" << endl;
+}
